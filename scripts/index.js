22a7a14e0dc6a0f8de82d8100e0ebb2d7f09bf6b
Mikhail Tiapkov <mixail.georgiew@yandex.ru>
2022-12-22 20:46:08 +0300

    Правки после 1го ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,36 @@
+// Задаются значения для попапа редактирования профиля
 const profileEdit = document.querySelector('.profile__edit-button');
 const profileClose = document.querySelector('.popup__close-button');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
 const popup = document.querySelector('.popup');
 const popupForm = popup.querySelector('.popup__form');
-const nameInput = popup.querySelector('.popup__formfield_type_name');
-const descriptionInput = popup.querySelector('.popup__formfield_type_description');
+const nameInput = popup.querySelector('.popup__form-field_type_name');
+const descriptionInput = popup.querySelector('.popup__form-field_type_description');
 
-function popupOpen() {
+//Функция открытия попапа редактирования профиля
+function openPopup() {
     popup.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
     descriptionInput.value = profileDescription.textContent;
 }
 
-
-function popupClose() {
+//Функция закрытия попапа редактирования профиля
+function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
-
+//Функция сохранения имени и описания профиля
 function saveProfile(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileDescription.textContent = descriptionInput.value;
-    popupClose();
+    closePopup();
   }
 
-profileEdit.addEventListener('click', popupOpen);
-profileClose.addEventListener('click', popupClose);
+//Кнопки открытия, закрытия и сохранения для профиля
+profileEdit.addEventListener('click', openPopup);
+profileClose.addEventListener('click', closePopup);
 popupForm.addEventListener('submit', saveProfile);
 
 
